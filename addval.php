040785ae74aa73b15b5d220a5e6ae6bf481c4933
xxxx7559 <xxxx7559@gmail.com>
2017-10-03 10:03:12 +0800

    Update addval.php

--- addval.php
+++ addval.php
@@ -1,16 +1,4 @@
-﻿<?php
-
-   $val= $_POST["val"];
- if($val == "1"){
-   $data = fopen("/Demo/val.json", "r+");
-    $dataArr=json_encode(array(   
-        
-        "nametext"=> $_POST["nametext"],   
-        "mailtext"=> $_POST["mailtext"],   
-        "valtextarea"=> $_POST["valtextarea"]
-    ));
-    fseek($data,-1,SEEK_END) ;
-    fwrite($data, ",".$dataArr."]");
+<?
     echo "OK";}
    
 ?>
